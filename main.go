f84c1cbbb5076d9661d66275152f2f01485733ee
cesaraugp <cesar.perez@sundevs.com>
2022-01-11 08:49:44 -0400

    FIX Public Routes

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 import (
 	mid "auth-service/pkg/middleware"
+	"auth-service/pkg/routes"
 	"auth-service/platform/database"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
-
-	"os"
 )
 
 func main() {
 	app := fiber.New()
 	mid.FiberMiddleware(app)
+	routes.PublicRoutes(app)
 	database.Init()
 	port := os.Getenv("PORT")
 	app.Listen(":" + port)
